14e506a5db53daf3e4f2eb11dc78d1389c94cfe5
RHPO <ramyhadid2005.rh@gmail.com>
2025-02-08 14:45:59 +0100

    Fix

--- codiha/.svelte-kit/generated/client/nodes/3.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/club/+page.js";
-export { universal };
-export { default as component } from "../../../../src/routes/club/+page.svelte";
--- codiha/src/routes/club/+page.js
+++
@@ -1,6 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-
-export function load() {
-    // ...
-    redirect(302, '/'); // needs `throw` in v1
-}
