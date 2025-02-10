35f873cba495b05a6a29b0244dc474980700e76c
RHPO <ramyhadid2005.rh@gmail.com>
2025-02-10 12:01:59 +0100

    Fixed Missing CORS

--- index.js
+++ index.js
@@ -31,6 +31,19 @@ app.use((req, res, next) => {
     return next(); // Skip proxy for these routes
   }
 
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  
+      // Request methods you wish to allow
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  
+      // Request headers you wish to allow
+      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  
+      // Set to true if you need the website to include cookies in the requests sent
+      // to the API (e.g. in case you use sessions)
+      res.setHeader('Access-Control-Allow-Credentials', true);
+  
+
 	const proxy = createProxyMiddleware({
 	  target: targetUrl,
 	  changeOrigin: true,
